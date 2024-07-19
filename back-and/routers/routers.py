4820ebb341201c3348d7902584eb7c2e93f1c62d
anapsantos1 <paulalimas@gmail.com>
2024-07-18 22:13:16 -0300

    feat: ajustes para o banco de dados

--- back-and/routers/routers.py
+++ back-and/routers/routers.py
@@ -1,23 +1,35 @@
 ##todas as rotas aqui
+import os
 from flask import Blueprint, request, jsonify
 from Controllers.login import AuthController
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 auth_blueprint = Blueprint('auth', __name__)
-auth_controller = AuthController()
+
+mongodb_uri = os.getenv("MONGODB_URL")
+client = MongoClient(mongodb_uri)
+database_name = os.getenv("DATABASE_NAME")
+db = client[database_name]
+
+auth_controller = AuthController(db)
 
 @auth_blueprint.route('/login', methods=['POST'])
 def login():
-    data = request.get_json()
-    email = data.get('Email')
-    password = data.get('Senha')
+    data = request.json
+    email = data.get('email')
+    password = data.get('password')
     response, status = auth_controller.login(email, password)
     return jsonify(response), status
 
 @auth_blueprint.route('/change-password', methods=['POST'])
 def change_password():
-    data = request.get_json()
-    email = data.get('Email')
-    current_password = data.get('SenhaAtual')
-    new_password = data.get('NovaSenha')
+    data = request.json
+    email = data.get('email')
+    current_password = data.get('current_password')
+    new_password = data.get('new_password')
     response, status = auth_controller.change_password(email, current_password, new_password)
     return jsonify(response), status
